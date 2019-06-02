44ed6cc09bb2a41fee145edfd17e35798d05175e
YeonwooSung <neos960518@gmail.com>
2019-06-02 13:02:26 +0900

    implement server

---
+++ src/app.js
@@ -0,0 +1,52 @@
+//import required libraries
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let app = express();
+
+let expo_sdk = require('expo-server-sdk');
+
+let Expo = expo_sdk.Expo;
+
+// Create a new Expo SDK client
+let expo = new Expo();
+
+
+const bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+
+
+//basic set ups
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.post('/notification', (req, res) => {
+    let data = req.body;
+    console.log(data);
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+
+// error handler
+app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports.app = app;
