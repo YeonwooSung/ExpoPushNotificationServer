98e2d57a0b60c49d6c09b7df157aab4db113c5c0
YeonwooSung <neos960518@gmail.com>
2019-06-03 15:39:41 +0900

    generate and send message chunks to the Expo push notification service

--- src/pushNotification.js
+++ src/pushNotification.js
@@ -27,6 +27,27 @@ let generateMessage = (token, msg, data) => {
     return message;
 }
 
-let generatePushNotifications = () => {
+let generatePushNotifications = (messages) => {
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+
+    // Send the chunks to the Expo push notification service.
+    // There are different strategies you could use.
+    // A simple one is to send one chunk at a time, which nicely spreads the load out over time.
+    for (let chunk of chunks) {
+        try {
+            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+            console.log(ticketChunk);
+            tickets.push(...ticketChunk);
+
+            // NOTE: If a ticket contains an error code in ticket.details.error, you
+            // must handle it appropriately. The error codes are listed in the Expo
+            // documentation:
+            // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     //
 }
